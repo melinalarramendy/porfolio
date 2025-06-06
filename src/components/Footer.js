8aa08d2079190189f090736ad8a2c0a13e55da99
Melina <melinalarramendy@gmail.com>
2025-06-06 19:28:08 -0300

    Arregle el footer en pantalla del celular

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,29 @@
 import { Container, Row, Col } from "react-bootstrap";
 import iconogithub from '../assets/img/icono-github.svg';
-import iconogmail from '../assets/img/icono-gmail.svg'; 
+import iconogmail from '../assets/img/icono-gmail.svg';
 import iconolinkedin from '../assets/img/icono-linkedin.svg';
 
 
 export const Footer = () => {
-    return (
-      <footer className="footer">
-        <Container>
-          <Row className="align-items-center justify-content-between text-center">
-            <Col xs="auto">
-              <h4>ML</h4>
-            </Col>
-            <Col xs="auto">
-              <p>Copyright 2025. All Rights Reserved</p>
-            </Col>
-            <Col xs="auto">
-              <div className="social-icon d-flex">
-                <a href="https://github.com/melinalarramendy"><img src={iconogithub} alt="GitHub Icon" /></a>
-                <a href="mailto:melinalarramendy@gmail.com?subject=Contacto desde Portfolio&body=Hola,%20me%20interesa%20trabajar%20juntos."><img src={iconogmail} alt="Gmail Icon" /></a>
-                <a href="https://www.linkedin.con/in/melina-larramendy-60340552b"><img src={iconolinkedin} alt="LinkedIn Icon" /></a>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </footer>
-    );
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="footer-row">
+          <Col xs={12} md="auto">
+            <h4>ML</h4>
+          </Col>
+          <Col xs={12} md="auto" className="mt-2">
+            <div className="social-icon">
+              <a href="https://github.com/melinalarramendy"><img src={iconogithub} alt="GitHub" /></a>
+              <a href="mailto:melinalarramendy@gmail.com?subject=Contacto desde Portfolio&body=Hola,%20me%20interesa%20trabajar%20juntos."><img src={iconogmail} alt="Email" /></a>
+              <a href="https://www.linkedin.com/in/melina-larramendy-60340552b"><img src={iconolinkedin} alt="LinkedIn" /></a>
+            </div>
+          </Col>
+          <Col xs={12} md="auto" className="mt-2">
+            <p>Copyright 2025. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
